ec77d8cd7c6d619508773bd8dd0631003eb7c71d
Wajahat Malik <wajahatmalik1179@gmail.com>
2024-12-06 18:59:40 +0500

    added second statement

--- classes_ij/com/designpatterns_mosh/Main.java
+++ classes_ij/com/designpatterns_mosh/Main.java
@@ -3,5 +3,7 @@ package com.designpatterns_mosh;
 public class Main {
     public static void main(String[] args) {
         System.out.println("First Statement");
+        System.out.println("Second Statement");
     }
+
 }
