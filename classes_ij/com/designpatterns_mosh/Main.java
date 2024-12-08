aac62c25538f1f5cb8356385ec0945c460bcf79f
Wajahat Malik <wajahatmalik1179@gmail.com>
2024-12-08 17:53:39 +0500

    third statement added

--- classes_ij/com/designpatterns_mosh/Main.java
+++ classes_ij/com/designpatterns_mosh/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First Statement");
         System.out.println("Second Statement");
+        System.out.println("Third Statement");
+
     }
 
 }
