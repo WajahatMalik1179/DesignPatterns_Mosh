1d048abb46bc60b71122b2152ed2479ba3f02557
Wajahat Malik <wajahatmalik1179@gmail.com>
2024-12-12 14:58:17 +0500

    Classes lecture done

--- classes_ij/com/designpatterns_mosh/Main.java
+++ classes_ij/com/designpatterns_mosh/Main.java
@@ -2,7 +2,9 @@ package com.designpatterns_mosh;
 
 public class Main {
     public static void main(String[] args) {
-        
+        User user = new User("Wajahat");
+        user.sayHello();
+
     }
 
 }
