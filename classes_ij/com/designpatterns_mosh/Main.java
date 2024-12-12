938fca9fc3c0265d39eb7a64f4074c17ff1bb008
Wajahat Malik <wajahatmalik1179@gmail.com>
2024-12-12 15:49:42 +0500

    Encapsulation Lecture Done

--- classes_ij/com/designpatterns_mosh/Main.java
+++ classes_ij/com/designpatterns_mosh/Main.java
@@ -2,10 +2,20 @@ package com.designpatterns_mosh;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\t\t***Classes***");
         User user = new User("Wajahat");
         user.sayHello();
+
+        System.out.println("\n\t\t***Interfaces***");
         TaxCalculator calculator = getCalculator();
         System.out.println(calculator.calculateTax());
+
+        System.out.println("\n\t\t***Encapsulation***");
+        var account = new Account();
+        account.deposit(10);
+        account.withdraw(5);
+        System.out.println(account.getBalance());
+
     }
 
     public static TaxCalculator getCalculator(){
